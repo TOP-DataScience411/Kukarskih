727bcd69044ced7d4e4a301a179d39d2e08f8520
SabakaBabaka228 <kukarskih29@gmail.com>
2024-11-04 20:56:56 +0500

    3th homework

---
+++ 2024.10.27/DZ3.py
@@ -0,0 +1,5 @@
+minuts = int(input("Enter minuts must be positive "))
+resultHours = minuts/60
+resultMin = minuts%60
+total = f"{minuts} minuts are {resultHours} hours and {resultMin} min" 
+print (total)
