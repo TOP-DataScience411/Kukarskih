e67815ffbac813e53280dabb19a55d2d5b72bce9
SabakaBabaka228 <kukarskih29@gmail.com>
2024-11-04 20:00:06 +0500

    Проверяю работу гита. Остальное допишу

---
+++ 2024.10.27/DZ1.py
@@ -0,0 +1,11 @@
+name = input("Enter name ")
+surname = input("Enter surname")
+birthday = 2024 - int(input("Enter your god rozhdenya"))
+
+print (last_name, first_name, howOld, year)
+    
+    
+   
+    
+
+
